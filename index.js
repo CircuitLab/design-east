4b2ebbe9376a6bc57a95c5f2af87f49cf53ee27a
nulltask <nulltask@gmail.com>
2013-08-09 21:03:43 +0900

    open url after listening

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 var mecab = require('mecab-ffi');
 var server = require('http').createServer(app);
 var io = require('engine.io').attach(server);
+var opener = require('opener');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
@@ -15,7 +16,7 @@ var tu = tuiter(keys);
 tu.filter({ track: ['#拡散希望'] }, function(stream) {
   stream.on('tweet', function(tweet) {
     mecab.parse(tweet.text, function(err, result) {
-      console.log(err, result, tweet.text);
+      // console.log(err, result, tweet.text);
       tweet.segments = result;
       Object.keys(io.clients).forEach(function(key) {
         io.clients[key].send(JSON.stringify(tweet));
@@ -26,4 +27,5 @@ tu.filter({ track: ['#拡散希望'] }, function(stream) {
 
 server.listen(app.get('port'), function() {
   console.log('listening in port %s', app.get('port'));
+  opener('http://localhost:' + app.get('port') + '/');
 });
